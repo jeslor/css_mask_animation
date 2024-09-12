235bafa28945bd798d37d1ab3083bf594d405bd8
twani <jbestius@gmail.com>
2024-09-12 18:17:46 +0200

    house keeping on initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="h-screen w-screen bg-[#18181a] flex justify-center items-center">
+      <h1 className="text-5xl text-slate-300">CSS MAsk animation</h1>
+
+    </div>
+  )
+}
+
+export default App
